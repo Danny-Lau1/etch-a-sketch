417bd576db0c048c856f77c6a8cf89717e9e21fa
Danny Lau <dnnlau@gmail.com>
2022-10-18 14:10:00 -0700

    Add basic grid layout

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,26 +4,33 @@
 const container = document.querySelector('.container')
 
 createGrid()
-
+draw()
 
 function createGrid() {
 
     for (let i = 0; i < 16; i++) {
         const column = document.createElement('div')
-        container.appendChild(column)
         column.classList.add('column')
-        column.textContent = "column"
-        console.log(column)
+        container.appendChild(column)
 
 
-        for (let j = 0; j < 15; j++) {
+        for (let j = 0; j < 16; j++) {
             const row = document.createElement('div')
-            column.appendChild(row)
-            row.textContent = "row"
             row.classList.add('row')
-            console.log(row)
+            column.appendChild(row)
 
 
         }
     }
 }
+
+function draw() {
+    const rows = document.querySelectorAll('div.row')
+    rows.forEach((row) => {
+        row.addEventListener('mouseover', () => {
+            row.setAttribute('style', 'background-color: black')
+        })
+    })
+}
+
+
