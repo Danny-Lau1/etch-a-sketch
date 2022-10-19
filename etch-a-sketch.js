5a5749fc799919496c295eaed850a9af2d6f58bc
Danny Lau <dnnlau@gmail.com>
2022-10-19 04:21:16 -0700

    Add drawing, update CSS to fit better frame, Add buttons and grid sizes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,31 @@
 let size = 20
+let styleName = 'background-color'
+let color = 'black'
+
+
+// Capturing Buttons
 const container = document.querySelector('.container')
 const sizeButtons = document.querySelectorAll('.sizeButton')
-const eraseButton = document.getElementById("erase")
+const resetButton = document.getElementById('reset')
+const eraseButton = document.getElementById('erase')
+const blackButton = document.getElementById('black')
 
+// EventListeners for each button
+resetButton.addEventListener('click', reset)
+eraseButton.addEventListener('click', erase)
+blackButton.addEventListener('click', drawBlack)
 
-// Default size with drawing function 
-createGrid(size)
-draw()
-
+// Get user's input on size of gird
 sizeButtons.forEach((button) => {
     button.addEventListener('click', gridSize)
 })
 
+// Default size with drawing function 
 
 
 
 
+// Passes user's input fize to create grid of specific size
 function gridSize(button) {
     size = this.id
     createGrid(size)
@@ -39,16 +49,37 @@ function createGrid(size) {
 
         }
     }
-    draw()
+
 }
 
-function draw() {
+function drawBlack(color) {
+    console.log('click')
     const rows = document.querySelectorAll('div.row')
     rows.forEach((row) => {
         row.addEventListener('mouseover', () => {
-            row.setAttribute('style', 'background-color: black')
+            row.style.backgroundColor = color
         })
     })
 }
 
+function reset() {
+    container.innerHTML = ''
+    createGrid(size)
+    drawBlack(color)
+}
 
+function erase() {
+    const rows = document.querySelectorAll('div.row')
+    rows.forEach((row) => {
+        row.addEventListener('mouseover', () => {
+            row.setAttribute('style', 'background-color: white')
+        })
+    })
+}
+
+function start(size, color) {
+    console.log('hello')
+    createGrid(size)
+    drawBlack(color)
+}
+start(size, color)
